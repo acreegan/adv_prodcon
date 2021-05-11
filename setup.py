74cda50a47fbffb994235916331a3caa87ba7ea2
Andrew <andrew.s.creegan@gmail.com>
2021-05-12 11:13:51 +1200

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/acreegan/adv_prodcon',
-    version='0.1.5',
+    version='0.1.6',
     zip_safe=False,
 )
 
